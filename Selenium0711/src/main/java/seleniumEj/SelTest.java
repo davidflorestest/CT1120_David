b1b80192431c3cf88fc4fdbfc8e0fc35f8c82e34
David Flores <David Flores@192.168.1.49>
2020-11-14 08:49:42 -0600

    First commit

---
+++ Selenium0711/src/main/java/seleniumEj/SelTest.java
@@ -0,0 +1,20 @@
+package seleniumEj;
+
+import java.io.File;
+import java.nio.file.Paths;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SelTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Test");
+		String exePath = Paths.get("").toAbsolutePath().toString() + File.separator + "drivers" + File.separator;
+		System.setProperty("webdriver.chrome.driver", exePath + "chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.google.com/");
+	}
+
+}
