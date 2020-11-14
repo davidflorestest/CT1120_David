7a1f44f7d7677bc96d4ef32312885a7687cc5852
davidflorestest <74459472+davidflorestest@users.noreply.github.com>
2020-11-14 09:17:50 -0600

    Update SelTest.java
    Update Class

--- Selenium0711/src/main/java/seleniumEj/SelTest.java
+++ Selenium0711/src/main/java/seleniumEj/SelTest.java
@@ -9,6 +9,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class SelTest {
 
 	public static void main(String[] args) {
+		
 		// TODO Auto-generated method stub
 		System.out.println("Test");
 		String exePath = Paths.get("").toAbsolutePath().toString() + File.separator + "drivers" + File.separator;
